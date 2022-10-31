e71d38c4d4c091271b38ace1cb5aaaee5ceb53fb
Miika Kylmänen <kylmanen.miika@gmail.com>
2022-10-31 21:44:33 +0200

    Tuoteryhmä komponentti desktop-näkymästä tehty

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,18 +1,25 @@
 import '../styles/frontpage.css'
 import AdvertisementBanner from './AdvertisementBanner'
+import ProductCategory from './ProductCategory'
+
+import kavelyImage from '../Kuvat/Sivustokuvat/KengätArtboard2.jpg'
+import koripalloImage from '../Kuvat/Sivustokuvat/KengätArtboard3.jpg'
+import skeittiImage from '../Kuvat/Sivustokuvat/KengätArtboard4.jpg'
 
 const FrontPage = () => {
     return (
         <div className="container-fluid frontpage-container">
-            <div className="row">
-                <div className="cover">
+            <div className="row mx-0 my-1">
+                <div className="cover p-0 col-lg-12">
                 </div>
             </div>
-            <div className="row">
+            <div className="row mx-0 my-1">
                 <AdvertisementBanner />
             </div>
-            <div className="row">
-                
+            <div className="row mx-0 my-1 flex-lg-nowrap">
+                <ProductCategory image={kavelyImage} categoryName={"Kävelykengät"}/>
+                <ProductCategory image={koripalloImage} categoryName={"Koripallokengät"} />
+                <ProductCategory image={skeittiImage} categoryName={"Skeittikengät"} />
             </div>
         </div>
     );
---
+++ src/components/ProductCategory.js
@@ -0,0 +1,14 @@
+import '../styles/productCategory.css'
+
+const ProductCategory = (props) => {
+    return (
+        <div className="col-lg-4 m-0 p-0 d-flex justify-content-center align-items-center"
+        style={{background: 'url' + '(' + props.image + ')', backgroundSize: 'cover',height:'60vh'}}>
+            <div className="imageOverlay d-flex justify-content-center align-items-center">
+                <h1>{props.categoryName}</h1>
+            </div>
+        </div>
+      );
+}
+ 
+export default ProductCategory;
