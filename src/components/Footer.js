514b701a0bbf6404ecb182ade4e587d9913fcfc9
Tuomasromppanen <tuomas.romppanen@outlook.com>
2022-11-09 21:19:43 +0200

    Kotisivun responssivuus tehty

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from '../Kuvat/Sivustokuvat/Collect_shoes.png'
 const Footer = () => {
     return (
         <div className="container-fluid footer d-flex justify-content-center">
-          <img id="logo2" src={logo}></img>
+          <img id="logo2" src={logo} alt=""></img>
         </div>
       );
 }
