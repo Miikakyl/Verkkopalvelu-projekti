b041f6752e250433629fa2dba2d7792a77b3361a
Miikakyl <kylmanen.miika@gmail.com>
2022-11-29 14:17:38 +0200

    Hakukentästä tehty oma komponentti ja tyylitiedosto. Axios lisätty myös

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import Logo from '../Kuvat/Sivustokuvat/Collect_shoes.png'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Shoppingcart from './Shoppingcart'
+import Searchbar from './Searchbar.js'
 import LoginForm from './LoginForm'
 
 /*
@@ -48,15 +49,7 @@ const Nav = () => {
             </li>
           </ul>
         </div>
-        <form className="inputForm d-flex position-relative" role="search">
-          <input className="searchInput mx-2" type="text" placeholder="Haku" aria-label="Search">
-          </input>
-          <button id="searchBtn" onClick={() => { }}>
-            <svg id="searchIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </button>
-        </form>
+        <Searchbar />
         <div className="d-flex flex-row buttonContainer">
           <LoginForm />
           <Shoppingcart />
