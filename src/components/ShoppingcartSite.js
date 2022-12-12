5db1e05253c432f9dea54d1f3fbb42f354b91a92
AkseliRaj <akseli.rajahalme@gmail.com>
2022-12-12 13:05:32 +0200

    Added increment/decrement buttons

--- src/components/ShoppingcartSite.js
+++ src/components/ShoppingcartSite.js
@@ -1,6 +1,8 @@
 import "../styles/ShoppingcartSite.css"
 import { Link } from "react-router-dom";
 import React,{ useEffect } from "react";
+import { useState } from "react";
+
 
 const ShoppingcartSite = (props) => {
 
@@ -11,7 +13,45 @@ const ShoppingcartSite = (props) => {
         props.navbarHidingState(false)
      }
    }, [])
+
+
+    const [counter, setCounter] = useState(1);
+    const incrementCounter = () => setCounter(counter + 1);
+    let decrementCounter = () => setCounter(counter - 1);
+
+    if(counter<=0) {
+        decrementCounter = () => setCounter(1);
+    }
    
+    function ButtonIncrement(props) {
+  
+        return (
+          <button onClick={props.onClickFunc} id="decrementButton">
+          -
+          </button>
+        )
+     }
+
+     function ButtonDecrement(props) {
+  
+        return (
+          <button onClick={props.onClickFunc} id="incrementButton">
+          +
+          </button>
+        )
+      }
+
+      function Display(props) {
+        return (
+            
+          <label >{props.message}</label>
+        )
+      }
+
+      if(counter<=1) {
+        decrementCounter = () => setCounter(1);
+      }
+
     
     return (
         <div id="Shoppingcart">
@@ -38,31 +78,10 @@ const ShoppingcartSite = (props) => {
                         <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center">
                             <p>Väri: musta</p>
                         </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center" >
-                            <p id="size">*Valitse koko*</p>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 d-flex justify-content-center" id="remove">
-                            <button id='removeButton'><i class="bi bi-trash"></i></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container" id="order">
-                    <div class="row d-flex align-items-center justify-content-center" >
-                        <div class="col-lg-2">
-                            <img src={require('../Kuvat/Tuotekuvat/Paivitetyt_Tuotekuvat/Nike_AirForce_1/Nike_AirForce_1_1.png')} alt="" id="image"/>
-                        </div>
-                        <div class="col-lg-2 text-center" id='productName'>
-                            <p>Air Force 1: <span>100,00€</span></p>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center" id="">
-                            <p>Koko: 32</p>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center">
-                            <p>Väri: musta</p>
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center" >
-                            <p id="size">*Valitse koko*</p>
+                        <div class="col-lg-2 col-md-6 col-sm-6 col-6 text-center d-flex justify-content-center" id="amount">
+                            <ButtonIncrement onClickFunc={decrementCounter} />
+                                    <Display message={counter} />
+                            <ButtonDecrement onClickFunc={incrementCounter} />
                         </div>
                         <div class="col-lg-2 col-md-6 col-sm-6 col-6 d-flex justify-content-center" id="remove">
                             <button id='removeButton'><i class="bi bi-trash"></i></button>
@@ -71,7 +90,7 @@ const ShoppingcartSite = (props) => {
                 </div>
 
                 <div id='total' class="col-12">
-                    <p id="totalText">Yhteensä: 1200€</p>
+                    <p id="totalText">Yhteensä: <span>1200€</span></p>
                 </div>
 
                 <div class="col-12 d-flex justify-content-center m-b-5">
